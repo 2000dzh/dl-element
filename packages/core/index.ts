eb2181adc4fb19827598c13c60c9f7534d158411
Ddzh <DddZzHh@163.com>
2024-10-08 13:18:59 +0800

    优化配置

--- packages/core/index.ts
+++ packages/core/index.ts
@@ -7,5 +7,5 @@ import '@dl-element/theme/index.css';
 library.add(fas)
 const installer = makeInstaller(componens);
 
-export * from '@dl-element/components';
+export * from '../components';
 export default installer;
